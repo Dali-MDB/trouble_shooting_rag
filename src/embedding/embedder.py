a0a7735086043ca98a2cfbdb71cadbed45d52d09
dali <dalibraham.18m@gmail.com>
2026-08-15 18:19:59 +0100

    Implemented Embedder

---
+++ src/embedding/embedder.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+from chunking.models import Chunk
+
+class Embedder:
+    def __init__(self):
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def embed(self, chunks: list[Chunk])->list[tuple[Chunk, list[float]]]:
+        texts = [c.content for c in chunks]
+        vectors = self.model.encode(texts)
+        #pair the chunks with their corresponding vectors
+        return  [(chunk, vector.tolist()) for chunk, vector in zip(chunks, vectors)]
